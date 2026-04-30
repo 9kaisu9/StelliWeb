660f4010f11c2535a5db508f8f64bd94d189704e
Kai Su <kaisu@Mac.fritz.box>
2026-04-30 18:14:42 +0200

    first commit

---
+++ stelli-backend/src/main/java/com/stelli/stelli_backend/StelliBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stelli.stelli_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StelliBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StelliBackendApplication.class, args);
+	}
+
+}
---
+++ stelli-backend/src/test/java/com/stelli/stelli_backend/StelliBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stelli.stelli_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StelliBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
